d5df9a2e6c2ec3494eeb4b458472abdacc7d2389
seanrogan <srogan88@gmail.com>
2022-05-01 14:00:45 -0400

    moved .git to parent dir

--- src/http-common.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: "https://loaclhost:8080/api",
-    headers: {
-        "Content-type":"application/json"
-}
-});
---
+++ src/test/java/com/seanrogandev/crudapp/demo/CrudDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanrogandev.crudapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
