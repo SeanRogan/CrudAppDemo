916e26d846b4533f349cc2bfdc767324f811b48b
seanrogan <srogan88@gmail.com>
2022-05-01 13:11:00 -0400

    trying to integrate backend and frontend

--- demo-backend/src/main/java/com/seanrogandev/crudapp/demo/repository/TutorialRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seanrogandev.crudapp.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.seanrogandev.crudapp.demo.model.Tutorial;
-import java.util.List;
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
-    List<Tutorial> findByTitleContaining(String title);
-}
